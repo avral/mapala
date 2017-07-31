87ae0368b58c4d39139072515f32b1a3e9dbca70
avral <qweasd_96@mail.ru>
2017-08-01 01:19:37 +0300

    hide ico

--- frontend/base/Top.vue
+++ frontend/base/Top.vue
@@ -35,9 +35,12 @@
               Настройки
             </router-link>
 
+
+            <!-- Закрыто до возобновления работы шлюза
             <router-link class="m_item" :to="'/ico/'">
               ICO
             </router-link>
+            -->
 
             <!-- <a class="m_item" href="">FAQ</a> -->
 
