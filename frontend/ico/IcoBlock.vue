fe2c7a8c9346ac7fae57a783b8c77975cd6562e6
Andrey Skurlatov <a.skurlatov@gmail.com>
2017-08-01 16:03:25 +0300

    Ico page blocked

---
+++ frontend/ico/IcoBlock.vue
@@ -0,0 +1,24 @@
+<template>
+    <div class="icoBlock">
+        <h1>Приостановлено до 04.08.2017 в связи с переходом на SegWit</h1>
+    </div>
+</template>
+
+<script>
+
+</script>
+
+<style scoped>
+    .icoBlock{
+        margin-top: -40px;
+        text-align: center;
+        position: relative;
+        top: 50%;
+        height: 100vh;
+        background: #4D7090 url('../assets/mapala.gif') no-repeat center center;
+        background-size: 30%;
+    }
+    h1{
+        padding-top: 50px;
+    }
+</style>
