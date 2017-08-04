23648b4df62d2995f744246c30e1b15463a8a259
avral <qweasd_96@mail.ru>
2017-08-04 14:37:53 +0300

    refactoring locale middleware

--- backend/middleware.py
+++ backend/middleware.py
@@ -1,8 +1,4 @@
-from django.contrib.auth.middleware import get_user
-from django.utils.functional import SimpleLazyObject
-from rest_framework_jwt.authentication import JSONWebTokenAuthentication
-
-from backend.settings import LOCALE
+from backend import settings
 
 
 class LocaleMiddleware(object):
@@ -10,22 +6,8 @@ class LocaleMiddleware(object):
         self.get_response = get_response
 
     def __call__(self, request):
-        request.user = SimpleLazyObject(lambda: self.__class__.get_jwt_user(request))
-
-        if request.user.is_authenticated():
-            LOCALE = request.user.locale
-        else:
-            # По дефотлу Русский блокчейн
-            LOCALE = 'ru'
+        # Сохраняем локаль клиента
+        if '/api/' in request.path:
+            settings.LOCALE = request.META.get('HTTP_LOCALE') or 'ru'
 
         return self.get_response(request)
-
-    @staticmethod
-    def get_jwt_user(request):
-        user = get_user(request)
-        if user.is_authenticated:
-            return user
-        jwt_authentication = JSONWebTokenAuthentication()
-        if jwt_authentication.get_jwt_value(request):
-            user, jwt = jwt_authentication.authenticate(request)
-        return user
