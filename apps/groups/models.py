92fc131fd442c7402583c1eb605b166a563ab88f
avral <qweasd_96@mail.ru>
2017-08-17 16:30:53 +0300

    add chanel link for group

--- apps/groups/models.py
+++ apps/groups/models.py
@@ -5,6 +5,7 @@ class Group(models.Model):
     name = models.CharField(max_length=200, null=True, unique=True)
     title = models.CharField(max_length=200, null=True)
     chat_link = models.CharField(max_length=200, null=True)
+    chanel_link = models.CharField('чат афиши', max_length=200, null=True)
     logo = models.ImageField(upload_to='groups/', null=True, blank=True)
     city = models.CharField(max_length=200, null=True)
 
