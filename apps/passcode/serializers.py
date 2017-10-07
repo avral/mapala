d1df948db2372a48f816b1760b418e976cca957e
avral <qweasd_96@mail.ru>
2017-10-07 23:03:32 +0300

    some security updates

--- apps/passcode/serializers.py
+++ apps/passcode/serializers.py
@@ -1,5 +1,6 @@
 from datetime import datetime, timedelta
 
+from phonenumbers import parse, format_number, PhoneNumberFormat
 from rest_framework import serializers
 
 from apps.common.mixins import ReCapchaMixin
@@ -14,22 +15,24 @@ class PassRequestSerializer(ReCapchaMixin, serializers.ModelSerializer):
         model = PassRequest
         fields = 'id', 'number', 'g_recaptcha_response'
 
-    def validate_number(self, data):
-        if User.objects.filter(number=data).exists():
+    def validate_number(self, number):
+        number = format_number(parse(number), PhoneNumberFormat.E164)
+
+        if User.objects.filter(number=number).exists():
             raise serializers.ValidationError(
                 'User with this phone already exists: %s' %
-                User.objects.get(number=data).username)
+                User.objects.get(number=number).username)
 
         # Отправить код можно только раз в 5 минут.
         active_time = datetime.now() - timedelta(minutes=5)
 
         if PassRequest.objects.filter(
-                number=data,
+                number=number,
                 created_at__gt=active_time):
 
             raise serializers.ValidationError('SMS can be sent in 5 minutes')
 
-        return data
+        return number
 
 
 class PassRequestValidateSerializer(serializers.Serializer):
