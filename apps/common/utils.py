8f8b816c34f6a655318fbe69f42f2e45ffa13658
avral <qweasd_96@mail.ru>
2017-10-10 15:08:03 +0300

    implement ScheduleArray class

--- apps/common/utils.py
+++ apps/common/utils.py
@@ -1,4 +1,6 @@
 import io
+import time
+from datetime import datetime, timedelta
 import PIL.Image
 
 from alphabet_detector import AlphabetDetector
@@ -115,3 +117,41 @@ def get_client_ip(request):
         ip = request.META.get('REMOTE_ADDR')
 
     return ip
+
+
+class ScheduleArray:
+    """
+    returns items with the specified delay.
+    items need to be list or tuples [(item: delay)], delay can be 0
+
+    delay should be in seconds
+    """
+
+    def __init__(self, items=None):
+        self.items = [
+            (i[0], self._delay(i[1]))
+            for i in items
+        ] if items else []
+
+    def __iter__(self):
+        return self
+
+    def _delay(self, seconds):
+        return datetime.now() + timedelta(seconds=seconds)
+
+    def append(self, item, delay):
+        ready_time = self._delay(delay)
+
+        self.items.append((item, ready_time))
+
+    def __next__(self):
+        while self.items:
+            try:
+                item = next(i for i in self.items if i[1] <= datetime.now())
+                self.items.remove(item)
+
+                return item[0]
+            except StopIteration:
+                time.sleep(0.1)
+
+        raise StopIteration
